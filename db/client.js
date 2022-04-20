5ad18325300936cc249e0927f99dd4da5584b235
Pam <take_asi@hotmail.com>
2022-04-20 13:56:32 -0500

    fixed drop table

--- db/client.js
+++ db/client.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 const LOGIN = process.env.REACT_APP_DB_login;
 const DB_NAME = 'blueBox';
 let client
-const DB_URL = process.env.DATABASE_URL || `postgres://${LOGIN}@localhost:5432/${DB_NAME}`;
+const DB_URL = process.env.DATABASE_URL || `postgres://localhost:5432/${DB_NAME}`;
 
 // github actions client config
 if (process.env.CI) {
