865814007b9caf62ddcf3124b1786e33df993b11
Josh S Wilkinson <JoshsWilkinson@Outlook.com>
2022-04-20 14:03:32 -0500

    fixed env login

--- db/client.js
+++ db/client.js
@@ -2,9 +2,17 @@
 const { Client } = require('pg');
 require("dotenv").config();
 const LOGIN = process.env.REACT_APP_DB_login;
-const DB_NAME = 'blueBox';
+//----------------------------------------------------------------
+let DB_NAME
+if (!LOGIN.length){
+  DB_NAME = `postgres://localhost:5432/blueBox`;
+} else {
+  DB_NAME = `postgres://${LOGIN}@localhost:5432/blueBox`;
+}
+//----------------------------------------------------------------
 let client
-const DB_URL = process.env.DATABASE_URL || `postgres://localhost:5432/${DB_NAME}`;
+
+const DB_URL = process.env.DATABASE_URL || `${DB_NAME}`;
 
 // github actions client config
 if (process.env.CI) {
