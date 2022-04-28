e98a9afee330421478ffa63fc2f242684dca49b5
Pam <take_asi@hotmail.com>
2022-04-28 12:00:08 -0500

    ready to connect

--- src/components/Main/Lists.jsx
+++ src/components/Main/Lists.jsx
@@ -4,21 +4,32 @@ const Lists = () => {
   return (
     <div className="lists">
       <ul>
-        <li>Movies Title</li>
+        <li>All</li>
+        <li>Movies</li>
+        <li>TV Shows</li>
+        <li>Rented</li>
       </ul>
+      <div className="title-container">
+        <ul className="title">
+        <h3>Title</h3>
+        <h4>RATE</h4>
+        </ul>
+      </div>
       <div className="detail-container">
         <ul>
           <il>Year -</il>
           <il> Type -</il>
-          <il> Duration</il>
+          <il> Duration -</il>
+          <li> Price </li>
         </ul>
       </div>
-    <div className="flex-container">
-      <div className="flex-child poster">
-        <p>"Link Poster" </p>
-      </div>
-      <div className="flex-child description">
-        {/* <p>Lorem ipsum dolor sit amet, 
+      <div className="flex-container">
+        <div className="flex-child poster">
+          <p>Link Poster </p>
+        </div>
+        <div className="flex-child description">
+          <p> Movie description</p>
+          {/* <p className="text-break">Lorem ipsum dolor sit amet, 
             consectetur adipiscing elit, 
             sed do eiusmod tempor incididunt 
             ut labore et dolore magna aliqua. 
@@ -31,25 +42,25 @@ const Lists = () => {
             sint occaecat cupidatat non proident, 
             sunt in culpa qui officia deserunt mollit 
             anim id est laborum.</p> */}
+        </div>
+      </div>
+
+      <div className="wishlist-bottom-container">
+        <button className="wishlist-bottom" onClick={() => updateDev()}>
+          Add to Wishlist
+        </button>
+        <button
+          className="wishlist-bottom"
+          onClick={() => {
+            addItemToCart(me.id, { item1337v2: `leetItemButBetter` });
+            setCartChange(true);
+          }}
+        >
+          {" "}
+          Add To Cart
+        </button>
       </div>
     </div>
-
-    <div className="wishlist-bottom-container">
-           <button
-            className=""
-            onClick={() => updateDev()}>
-              Wish List
-            </button>
-          <button
-            className=""
-            onClick={() => {
-              addItemToCart(me.id, { item1337v2: `leetItemButBetter` });
-              setCartChange(true);
-            }}
-          > Add To Cart</button>
-        </div>
-    </div>
-    
   );
 };
 
