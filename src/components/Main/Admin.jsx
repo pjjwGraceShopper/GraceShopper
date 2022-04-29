28678f5c4190f2ea5c57bc6c49e1a6e2e73914d5
Wyatt Wilson <wyattwilson916@gmail.com>
2022-04-29 11:14:10 -0400

    Added styling to login and sign up and admin page

--- src/components/Main/Admin.jsx
+++ src/components/Main/Admin.jsx
@@ -3,9 +3,9 @@ import { getUsers } from "../../axios-services/users_ajax";
 
 const Admin = () => {
   const [users, setUsers] = useState([]);
-  let admin = false
-  if(localStorage.getItem("isAdmin")){
-      admin = true
+  let admin = false;
+  if (localStorage.getItem("isAdmin")) {
+    admin = true;
   }
 
   useEffect(() => {
@@ -21,28 +21,30 @@ const Admin = () => {
         <>
           <h1>Admin Page</h1>
           <h2>All Users Info</h2>
-          <h4>______________________________</h4>
+              <table table dark striped hover>
+                <thead>
+                  <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">First Name</th>
+                    <th scope="col">Last Name</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">Username</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
           {users.map((user) => (
             <div key={user.id}>
-              <ul>
-                <li>
-                  <h5>ID Number: {user.id}</h5>
-                </li>
-                <li>
-                  <h6>Username: {user.username}</h6>
-                </li>
-                <li>
-                  <h6>Email: {user.email}</h6>
-                </li>
-                <li>
-                  <h6>First name: {user.firstname}</h6>
-                </li>
-                <li>
-                  <h6>Last name: {user.lastname}</h6>
-                </li>
-              </ul>
-            </div>
-          ))}
+                    <th scope="row">{user.id}</th>
+                    <td>{user.firstname}</td>
+                    <td>{user.lastname}</td>
+                    <td>{user.email}</td>
+                    <td>{user.username}</td>
+                  </div>
+                  ))}
+                  </tr>
+                </tbody>
+              </table>
         </>
       ) : null}
     </div>
--- src/components/Main/Login.jsx
+++ src/components/Main/Login.jsx
@@ -24,24 +24,24 @@ const Login = ({ me, setMe, loginStatus, setLoginStatus }) => {
       localStorage.setItem("token", result.token);
       localStorage.setItem("username", result.user.username);
       localStorage.setItem("id", result.user.id);
-      if(result.user.admin){
-        localStorage.setItem("isAdmin", result.user.admin)
+      if (result.user.admin) {
+        localStorage.setItem("isAdmin", result.user.admin);
       }
       // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
       navigate("/");
-      console.log('is result from login: ', me.id)
+      console.log("is result from login: ", me.id);
     }
   };
-  
+
   const onLogOut = async (e) => {
     e.preventDefault();
     localStorage.removeItem("token");
     localStorage.removeItem("username");
-    localStorage.removeItem('id')
-    localStorage.removeItem("isAdmin")
+    localStorage.removeItem("id");
+    localStorage.removeItem("isAdmin");
     console.log("Username and token removed from localStorage!");
     setLoginStatus(false);
-    navigate('/')
+    navigate("/");
   };
 
   useEffect(() => {
@@ -52,40 +52,86 @@ const Login = ({ me, setMe, loginStatus, setLoginStatus }) => {
 
   return (
     <div>
-      {loginStatus ? (
-        <button onClick={onLogOut}>Log Out</button>
-      ) : (
-        <>
-          <form
-            onSubmit={(e) => {
-              onLogin(e);
-            }}
-          >
-            <input
-              value={username}
-              type="text"
-              placeholder="Username"
-              onChange={(e) => {
-                setUsername(e.target.value);
-              }}
-            />
-            <input
-              value={password}
-              type="password"
-              placeholder="password"
-              onChange={(e) => {
-                setPassword(e.target.value);
-              }}
-            />
-            <button type="submit">Login</button>
-          </form>
-          {loginMessage.error ? (
-            <>
-              <h3>{loginMessage.name}</h3>
-            </>
-          ) : null}
-        </>
-      )}
+      <div className="container py-3 h-100">
+        <div className="row d-flex justify-content-center align-items-center h-100">
+          <div className="col-12 col-md-0 col-lg-0 col-xl-0">
+            <div
+              className="card bg-dark text-white"
+              style={{ borderRadius: "1rem" }}
+            >
+              <div className="card-body p-10 text-center">
+                <div className="mb-md-0 mt-md-0 pb-0">
+                  {!loginStatus ? (
+                    <>
+                      <h2 className="fw-bold mb-0 mx-0 text-uppercase">
+                        Login
+                      </h2>
+                      <p className="text-white-50 mb-5">
+                        Please enter your login and password!
+                      </p>
+                      <form
+                        onSubmit={(e) => {
+                          onLogin(e);
+                        }}
+                      >
+                        <div className="form-outline form-white mb-4">
+                          <input
+                            value={username}
+                            type="username"
+                            placeholder="Username"
+                            id="typeUsernameX"
+                            className="form-control form-control-lg"
+                            onChange={(e) => {
+                              setUsername(e.target.value);
+                            }}
+                          />
+                          <label className="form-label" htmlFor="typeUsernameX">
+                            Username
+                          </label>
+                        </div>
+                        <div className="form-outline form-white mb-4">
+                          <input
+                            value={password}
+                            type="password"
+                            placeholder="Password"
+                            id="typePasswordX"
+                            className="form-control form-control-lg"
+                            onChange={(e) => {
+                              setPassword(e.target.value);
+                            }}
+                          />
+                          <label className="form-label" htmlFor="typePasswordX">
+                            Password
+                          </label>
+                        </div>
+                        <button
+                          className="btn btn-outline-light btn-lg px-5"
+                          type="submit"
+                        >
+                          Login
+                        </button>
+                      </form>
+                      {loginMessage.error ? (
+                        <>
+                          <h3>{loginMessage.name}</h3>
+                        </>
+                      ) : null}
+                    </>
+                  ) : (
+                    <button
+                      onClick={onLogOut}
+                      className="btn btn-outline-light btn-lg px-5"
+                      type="submit"
+                    >
+                      Log Out
+                    </button>
+                  )}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
