3f45c2c42c8b657a573a3a51376c6ce13ffd5a2e
Josh S Wilkinson <JoshsWilkinson@Outlook.com>
2022-04-29 06:21:51 -0500

    in the middle of popping FE data

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,18 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import React, { useState, useEffect } from "react";
-// getAPIHealth is defined in our axios-services directory index.js
-// you can think of that directory as a collection of api adapters
-// where each adapter fetches specific info from our express server's /api route
 import { getAPIHealth } from "../axios-services";
 import "../style/index.css";
 import "../style/App.css";
 import { Route, Routes } from "react-router-dom";
-
 import { Footer, Sidebar, Login, MyLibrary, SignUp, Home, Cart, Lists, Admin } from "./index";
 import * as bootstrap from "bootstrap"
-
+//---------------------------------------------------------------
 const App = () => {
-  const [APIHealth, setAPIHealth] = useState("");
+  //--------------------------------------------------------------
   const [me, setMe] = useState({});
-  const [cartChange, setCartChange] = useState(false);
+  const [cartChange, setCartChange] = useState(null);
   const [loginStatus, setLoginStatus] = useState(false);
-
+//----------------------------------------------------------------
   useEffect(() => {
     if (localStorage.getItem("token")) {
       const id = localStorage.getItem("id");
@@ -25,57 +21,20 @@ const App = () => {
         token: token,
         id: id,
       });
-    }
-
-    // follow this pattern inside your useEffect calls:
-    // first, create an async function that will wrap your axios service adapter
-    // invoke the adapter, await the response, and set the data
-    const getAPIStatus = async () => {
-      const { healthy } = await getAPIHealth();
-      setAPIHealth(healthy ? "api is up! :D" : "api is down :/");
-    };
-
-    // second, after you've defined your getter above
-    // invoke it immediately after its declaration, inside the useEffect callback
-    getAPIStatus();
-  }, []);
-
+    }}, [])
+//-----------------------------------------------------
   return (
     <div className='sidebar-container'>
-      <Sidebar loginStatus={loginStatus} />
+      <Sidebar loginStatus={loginStatus}/>
       <div className='app-container'>
-        {/* <div className="main_title">Hello, World!</div>
-        <p>API Status: {APIHealth}</p> */}
         <Routes>
           <Route path='/' element={<Home />} />
-          <Route
-            path='/login'
-            element={
-              <Login
-                setMe={setMe}
-                me={me}
-                loginStatus={loginStatus}
-                setLoginStatus={setLoginStatus}
-              />
-            }
-          />
-          <Route
-            path='/sign-up'
-            element={
-              <SignUp
-                setMe={setMe}
-                loginStatus={loginStatus}
-                setLoginStatus={setLoginStatus}
-              />
-            }
-          />
-          {/* sign-up route currently not working */}
-
+          <Route path='/login' element={ <Login setMe={setMe} me={me} loginStatus={loginStatus}  setLoginStatus={setLoginStatus}  /> } />
+          <Route path='/sign-up' element={ <SignUp setMe={setMe} loginStatus={loginStatus} setLoginStatus={setLoginStatus} />} />
           <Route path="/my-library" element={<MyLibrary />} />
           <Route path="/Lists" element={<Lists/>}/>
-           <Route path='/cart' element={<Cart me={me} cartChange={cartChange} setCartChange={setCartChange}/>} />
-           <Route path="/admin" element={<Admin />}/>
-
+          <Route path='/cart' element={<Cart me={me} cartChange={cartChange} setCartChange={setCartChange}/>} />
+          <Route path="/admin" element={<Admin />}/>
         </Routes>
         <Footer />
       </div>
