f804fa4f2b3f1630c94481879d84d1ac467ef204
Wyatt Wilson <wyattwilson916@gmail.com>
2022-05-02 15:39:58 -0400

    Added New movies input to frontEnd

--- src/axios-services/lib_ajax.js
+++ src/axios-services/lib_ajax.js
@@ -9,3 +9,21 @@ export async function getLibrary() {
     return err;
   }
 }
+
+export const addMovie = async (name, type, year, genre, length, price, img) => {
+  try {
+    const { data } = await axios.post(`/api/library/new`, {
+      name,
+      type,
+      year,
+      genre,
+      length,
+      price,
+      img,
+    });
+    return data;
+  } catch (err) {
+    console.error("Unable to add new movie to database", err);
+    return error;
+  }
+};
--- src/components/Main/Home.jsx
+++ src/components/Main/Home.jsx
@@ -2,17 +2,21 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import React, { useState, useEffect } from "react";
 import MovieList from "./MovieList";
 import { getLibrary } from "../../axios-services";
+import AddMovie from "./AddMovie";
 
-
-function Home({currentMovie, setCurrentMovie}) {
-
+function Home({ currentMovie, setCurrentMovie, movies, setMovies }) {
 
   return (
-    <div className='container-fluid movie-app'>
-      <div className='row'>
-        <MovieList currentMovie={currentMovie} setCurrentMovie={setCurrentMovie}/>
-
+    <div className="container-fluid movie-app">
+      <div className="row">
+        <MovieList
+          currentMovie={currentMovie}
+          setCurrentMovie={setCurrentMovie}
+        />
+        
+        
       </div>
+      <AddMovie movies={movies} setMovies={setMovies} />
     </div>
   );
 }
